0861a6fc60bf610edc0658b214f9f144a8b526f2
Larry Lingston <lingstonelarry@gmail.com>
2022-09-09 10:39:31 -0800

    Push yet again

---
+++ 2nd ever/Exercise/index.js
@@ -0,0 +1,17 @@
+let count = 0;
+let countel = document.getElementById("count-el");
+function increament(){
+    count = count + 1;
+    countel.innerText = count;
+}
+
+function decreament(){
+    count = count - 1;
+    countel.innerText = count;
+}
+function save(){
+    
+}
+save();
+
+
---
+++ 2nd ever/trial/index.js
@@ -0,0 +1,169 @@
+ // This is my first javascript 
+// let name = 'Mosh';
+// console.log(name);
+
+ //let fname ='larry';
+ //console.log(fname);
+
+// const interest_rate =0.3;
+// console.log(interest_rate);
+ 
+ //let age = 30;
+ //let isapproved = true;
+ //let firstname = undefined;
+ //let lastname = null;
+ //let selectedColor = null;
+ //let person = {
+   // name: 'Mosh',
+    //age: 30
+ //};
+ //console.log(person);
+ //person.name ='larry';
+ //console.log(person);
+ //let selection ='name';
+ //person[selection]='ali';
+ //console.log(person);
+
+ //let list=['larry','ali'];
+ //console.log(list[0]);
+ //list[3]='baba';
+ //console.log(list);
+
+ //function greet(fname,lname){
+ //   console.log('Hello ' +fname +' '+lname);
+// }
+
+ //greet('Larry','joe');
+ //greet('Ali','baba');
+
+
+
+//console.log('hi world');
+// list=['larry','kofi','hello'];
+// console.log(list);
+// console.log(list[3])
+// list[3]='apple';
+// console.log(list);
+// list[0]=undefined;
+// console.log(list);
+// let a = 5;
+// let b = 6;
+
+//console.log(a);
+//console.log(b);
+//console.log(a*b);
+
+// function greet(fname,lname){
+//   console.log('hello world ' + fname + lname)
+// };
+// greet('Larry')
+
+
+// function appreciation(Name){
+//   console.log(Name + ' you are appreciated')
+
+// };
+
+// appreciation('larmp');
+
+// function multiply(a,b){
+//   console.log(a*b)
+// };
+// multiply(5,10);
+
+
+// list=['Apple','bannana','Dog','Cat','jeep','bugati'];
+// console.log(list);
+
+// list[0]='fruit';
+// console.log(list);
+
+// function replace(){
+//   console.log(list[1]+' is a ' + list[0])
+
+// };
+
+// replace()
+
+// function multi(a, b){
+//   return a*b
+// }
+// console.log(multi(4,5))
+
+// let car ='jeep';
+
+// switch(car)
+// {
+//   case 'bugati':
+//     console.log('Damn fine car');
+//     break;
+//   case 'ferrari':
+//     console.log('Cute car');
+//     break;
+//   case 'ford':
+//     console.log('manageable');
+//     break;
+//   case 'jeep':
+//     console.log('noted!');
+//     break;
+//   default:
+//     console.log('done!');
+// }
+
+// let word='hello';
+// console.log(word);
+
+// function greet(_){
+//   var userName = prompt("Please Enter Your Name");
+//   if(userName=="victor"){
+//     alert('you are a champion');
+//   }
+ 
+//   else { alert('no meaning');
+    
+//   } if (userName=null) {
+//     alert('No null statement')
+//   }
+//   else {
+//     alert('no meaning');
+//   }
+//  if (userName==undefined) {
+//     alert('no empty ')
+//  } else {
+//    alert('no meaning');
+//  }
+// }
+
+
+//   var Namess = prompt("Please Enter Your Name:");
+//   if(Namess=="victor"){
+//     alert("you are a champion");
+//     if(Namess==""){
+//       alert("No Empty values");}
+//       else
+//       {var Namess = prompt("Please Enter Your Name For The Second Time:");}
+//   }
+//   else{
+//     alert("It has no meaning");
+// }
+function hello(){var Namess =prompt("Please Enter Your Name:");
+if(Namess=="victor"){
+  alert("you are a champion");
+}
+else if(Namess==""){
+  alert("No Empty values");
+  var Namess = prompt("Please Enter Your Name For The Second Time:");
+  if (Namess==""){
+  alert("Last chance was wasted in foolishness")}
+else{
+  alert("Thank You!")
+}}
+else{
+  alert("It has no meaning");}
+}
+hello();
+
+
+
+
+
